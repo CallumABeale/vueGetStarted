10d4a5c1b84ce5d7e8d656026a8dce5f59ead4a8
CallumABeale <callum.beale.098@gmail.com>
2020-06-13 22:11:23 +1000

    Vuetify / VueX / Firebase Tutorial start

---
+++ my-app/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+	<v-app>
+		<v-navigation-drawer v-model="sidebar" app> </v-navigation-drawer>
+
+		<v-toolbar app>
+			<div class="hidden-sm-and-up">
+				<v-toolbar-side-icon @click="sidebar = !sidebar"></v-toolbar-side-icon>
+			</div>
+			<v-toolbar-title>{{ appTitle }}</v-toolbar-title>
+			<v-spacer></v-spacer>
+		</v-toolbar>
+
+		<v-content></v-content>
+	</v-app>
+</template>
