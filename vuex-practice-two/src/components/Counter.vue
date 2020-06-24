157b1d7a0508c874e39cb11b9472f4922787393a
CallumABeale <callum.beale.098@gmail.com>
2020-06-24 18:24:03 +1000

    vueX final lesson commit

---
+++ vuex-practice-two/src/components/Counter.vue
@@ -0,0 +1,24 @@
+<template>
+	<div>
+		<p>
+			<button @click="increment">+</button>
+			<button @click="decrement">-</button>
+			{{ $store.state.count }}
+		</p>
+	</div>
+</template>
+
+<script>
+export default {
+	methods: {
+		increment() {
+			this.$store.commit('increment');
+			console.log(this.$store.state.count);
+		},
+		decrement() {
+			this.$store.commit('decrement');
+			console.log(this.$store.state.count);
+		},
+	},
+};
+</script>
